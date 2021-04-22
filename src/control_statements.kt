c0da1092b4337abd8d100378606f9be112b23145
leemutai <mutailee361@gmail.com>
2021-04-22 13:36:53 +0300

    hello

---
+++ src/control_statements.kt
@@ -0,0 +1,11 @@
+fun main() {
+ val age = 13
+ if (age<10){
+     println("you are too young to watch this movie")
+ } else if (age<13) {
+     println("you can watch this movie with a parent")
+ } else {
+     println("you can watch this movie")
+ }
+
+}
