e2227bf583ebd9aec63bfdc7dbd9bd042f822bb3
leemutai <mutailee361@gmail.com>
2021-04-22 12:44:06 +0300

    hello

---
+++ src/strings.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var rawString :String = "i am a Raw string!"
+    val escapedString :String ="i ma escaped String!\n"
+
+    println("Hello!"+escapedString)
+    println("Heyy!"+rawString)
+}
