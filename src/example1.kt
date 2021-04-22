a856f1602634097d2811b8bb8b673bfc53daddea
leemutai <mutailee361@gmail.com>
2021-04-23 00:13:45 +0300

    hello

---
+++ src/example1.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val number = 8
+    val result = if (number>0){
+        "positive number"
+    }
+    else if (number<0){
+        "negative number"
+    }
+    else {
+        "zero"
+    }
+    println("number is $result")
+}
