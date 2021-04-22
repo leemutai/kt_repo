8220986c3d1d496b02e0156da01cd3690e49c4b6
leemutai <mutailee361@gmail.com>
2021-04-23 00:17:08 +0300

    hello

---
+++ src/when.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val x:Int =20
+    when (x) {
+        1-> print("x==1")
+        2-> print("x==2")
+        else ->{//Note the block
+            print("x is neither 1 or 2")
+
+        }
+    }
+}
