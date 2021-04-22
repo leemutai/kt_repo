99f904b8694a8fbb739da68e8d8ff1e2cec0a569
leemutai <mutailee361@gmail.com>
2021-04-22 12:04:51 +0300

    hello

---
+++ src/basic.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val a:Int =10000
+    val d:Double=100.00
+    val f:Float=100.00f
+    val l:Long=10
+    val s:Short=10
+    val b:Byte=1
+
+    println("your Int value is"+a);
+    println("your Double value is"+d);
+    println("your Float value is"+f);
+    println("your Long value is"+l);
+    println("your Short value is"+s);
+    println("your Byte value is"+b);
+}
