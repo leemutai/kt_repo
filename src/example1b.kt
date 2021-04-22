2a5c6cf0a6895e9a831274a5eb0dc11d555d31cd
leemutai <mutailee361@gmail.com>
2021-04-23 00:14:52 +0300

    hello

---
+++ src/example1b.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val x:Int =7
+    when (x){
+        1,2 -> print("value of x is neither 1 nor 2")
+        else -> {//note the block
+            print("x is neither 1 nor 2")
+
+        }
+    }
+
+}
