13bc07bfbb31ea7cb0bbc35525c6651cddaae7fb
leemutai <mutailee361@gmail.com>
2021-04-27 00:05:02 +0300

    hello

---
+++ src/when_mul.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var dayOfWeek = 7
+    when(dayOfWeek) {
+        1 ->{
+            //Block
+            println("MOnday")
+            println("First Day of The week")
+        }
+        7 -> println("Sunday")
+        else -> println("Other Days")
+    }
+
+}
