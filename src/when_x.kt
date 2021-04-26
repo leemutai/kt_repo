9466b0e495eadb37f52c5c735df1ecaced76c69b
leemutai <mutailee361@gmail.com>
2021-04-27 00:07:10 +0300

    hello

---
+++ src/when_x.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var dayOfWeek =1
+    when(dayOfWeek){
+        1 -> println("Monday")
+        2 -> println("Tuesday")
+        3 -> println("Wednesday")
+        4 -> println("Thursday")
+        5 -> println("Friday")
+        6 -> println("Saturday")
+        7 -> println("Sunday")
+        else -> println("Invalid Day")
+
+    }
+}// it will display thursday
