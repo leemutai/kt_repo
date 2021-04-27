5f008c4770a295120c1ff9542e9246b8472f772f
leemutai <mutailee361@gmail.com>
2021-04-27 23:41:57 +0300

    hello

---
+++ src/bloc.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var dayOfWeek = 1
+    when(dayOfWeek){
+        1-> {
+            //Block
+            println("Monday")
+            println("First Day of The week")
+        }
+        7 -> println("Sunday")
+        else -> println("Other days")
+    }
+}
