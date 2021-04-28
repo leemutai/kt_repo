4450511e6f847641f8b469fd6efe689f0c3b12f6
leemutai <mutailee361@gmail.com>
2021-04-28 18:28:50 +0300

    hello

---
+++ src/lesson1b.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var primeNumbers= intArrayOf(2,3,5,7,11)
+    for (index in primeNumbers.indices){
+        println("primeNumbers(${index+1}): ${primeNumbers[index]}")
+    }
+    //#output
+
+}
